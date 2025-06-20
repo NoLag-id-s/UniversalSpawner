278d4dc2cfabbcd3cc119ca082e1d9da3fbcdfc5
NoLag-id <74305796+NoLag-id-s@users.noreply.github.com>
2025-06-21 03:15:18 +0800

    Update LoaderHubV1.lua

--- LoaderHubV1.lua
+++ LoaderHubV1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id-s/Candyblossomfinder/refs/heads/main/LoaderV1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id-s/Candyblossomhopper/refs/heads/main/LoaderV2.lua"))()
